fc48e3b15453885de822f31d2c2205b9f3ee5d18
lalitayadav27 <lalita27yadav2006@gmail.com>
2025-02-08 14:25:49 +0530

    q4.c
    .c

--- Assignment/q4.c
+++ Assignment/q4.c
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cout << "Enter the value  of n for matrix: ";
+    cin >> n;
+    int matrix[n][n];
+    int number = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = number++;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
