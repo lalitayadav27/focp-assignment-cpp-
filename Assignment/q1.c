bc5ba2e3886e93d702c4a4825489f48e0d2e7ed7
lalitayadav27 <lalita27yadav2006@gmail.com>
2025-02-08 13:54:18 +0530

    q1.c
    .c

--- Assignment/q1.c
+++ Assignment/q1.c
@@ -1,1 +1,31 @@
+//If a number is prime, print its factors
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n, i;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int count = 0;
+    for (i = 2; i < n; i++) {
+        if (n % i == 0) {
+            count++;
+        }
+    }
+
+    if (count == 0 && n > 1) {  
+        cout << n << " is a prime number.\n";
+        cout << "Factors: ";
+        for (i = 1; i <= n; i++) {  
+            if (n % i == 0) {
+                cout << i << " ";
+            }
+        }
+        cout << endl;
+    } else {
+        cout << n << " is not a prime number.\n";
+    }
+
+    return 0;
+}
